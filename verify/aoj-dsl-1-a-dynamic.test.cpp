0738475dd047bf9e4d4df533fd717040b9c50557
NyaanNyaan <suteakadapyon3@gmail.com>
2020-07-24 23:13:50 +0900

    a

---
+++ verify/aoj-dsl-1-a-dynamic.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM \
+  "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A"
+
+#include "../competitive-template.cpp"
+#include "../data-structure/dynamic-union-find.cpp"
+
+void solve() {
+  ini(N, Q);
+  DynamicUnionFind uf;
+  rep(_, Q) {
+    ini(c);
+    if (c == 0) {
+      ini(x, y);
+      uf.unite(x, y);
+    } else {
+      ini(x,y);
+      out(uf.same(x,y));
+    }
+  }
+}
---
+++ verify/aoj-dsl-1-b.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM \
+  "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_B"
+
+#include "../competitive-template.cpp"
+#include "../data-structure/union-find-with-potential.cpp"
+
+void solve() {
+  ini(N, Q);
+  UnionFindWithPotential<int> uf(N);
+  rep(_, Q) {
+    ini(c);
+    if (c == 0) {
+      ini(x, y, z);
+      uf.merge(y, x, z);
+    } else {
+      ini(x, y);
+      if (!uf.same(x, y))
+        out("?");
+      else
+        out(uf.diff(y, x));
+    }
+  }
+}
