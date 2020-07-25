4d2f3789e923db3e85d940812a5a6ccb16c34132
NyaanNyaan <suteakadapyon3@gmail.com>
2020-07-25 13:12:15 +0900

    fix bug

---
+++ verify/aoj-dsl-3-d-cartesiantree.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM \
+  "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_D"
+
+#include "../competitive-template.cpp"
+#include "../graph/cartesian-tree.cpp"
+#include "../graph/heavy-light-decomposition.cpp"
+void solve() {
+  ini(N, L);
+  vi a(N);
+  in(a);
+  vvi g;
+  int root;
+  tie(g, root) = CartesianTree<int>(a);
+  HeavyLightDecomposition<vvi> hld(g);
+  vi ans(N - L + 1);
+  rep(i, N - L + 1) { ans[i] = a[hld.lca(i, i + L - 1)]; }
+  out(ans);
+}
--- verify/aoj-dsl-5-b-bit2d.test.cpp
+++ verify/aoj-dsl-5-b-bit2d.test.cpp
@@ -7,7 +7,7 @@
 void solve() {
   ini(N);
   int L = 1000;
-  BinaryIndexedTree2D<int> bit(N + 1, N + 1);
+  BinaryIndexedTree2D<int> bit(L + 1, L + 1);
   rep(i, N) {
     ini(x1, y1, x2, y2);
     bit.imos(x1, y1, x2 - 1, y2 - 1, 1);
