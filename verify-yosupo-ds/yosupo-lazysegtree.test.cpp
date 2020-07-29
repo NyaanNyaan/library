19cc2cff8526e8a8a251c1d188e420cd25e6608a
NyaanNyaan <suteakadapyon3@gmail.com>
2020-07-30 03:11:31 +0900

    add verify

---
+++ verify-yosupo-ds/yosupo-lazysegtree.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include "../competitive-template.hpp"
+#include "../modint/montgomery-modint.hpp"
+#include "../segment-tree/lazy-segment-tree.hpp"
+
+using mint = LazyMontgomeryModInt<998244353>;
+
+void solve() {
+  using P = pair<mint, mint>;
+  auto f = [](P a, P b) { return P{a.first + b.first, a.second + b.second}; };
+  auto g = [](P a, P b) {
+    return P{a.first * b.first + a.second * b.second, a.second};
+  };
+  auto h = [](P a, P b) {
+    return P{a.first * b.first, a.second * b.first + b.second};
+  };
+  P ti = {0, 0};
+  P ei = {1, 0};
+  ini(N, Q);
+  V<P> a(N, {0, 1});
+  rep(i, N) in(a[i].first);
+
+  LST<P, P, decltype(f), decltype(g), decltype(h)> seg(a, f, g, h, ti, ei);
+
+  rep(_, Q) {
+    ini(cmd);
+    if (!cmd) {
+      inl(l, r, b, c);
+      seg.update(l, r, {b, c});
+    } else {
+      inl(l, r);
+      out(seg.query(l, r).first);
+    }
+  }
+}
