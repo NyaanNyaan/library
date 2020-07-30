75bf464c803eb0ad5810f1f65cfe4f2c9775ae15
NyaanNyaan <suteakadapyon3@gmail.com>
2020-07-31 01:19:37 +0900

    add verify

---
+++ fps/differential-equation.hpp
@@ -0,0 +1,17 @@
+#include "formal-power-series.hpp"
+
+// find f, saitsfying equation f' = g(f) mod x ^ deg
+template <typename mint>
+FormalPowerSeries<mint> DifferentialEquation(
+    function<FormalPowerSeries<mint>(FormalPowerSeries<mint>, int)> g,
+    function<FormalPowerSeries<mint>(FormalPowerSeries<mint>, int)> gprime,
+    mint f0, int deg) {
+  using fps = FormalPowerSeries<mint>;
+  fps f{f0};
+  for (int i = 1; i < deg; i <<= 1) {
+    fps r = (-gprime(f, i << 1)).integral().exp(i << 1);
+    fps h = ((g(f, i << 1) - gprime(f, i << 1) * f) * r).pre(i << 1).integral();
+    f = ((h + f0) * r.inv(i << 1)).pre(i << 1);
+  }
+  return f.pre(deg);
+}
---
+++ verify-yuki/yuki-0963.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://yukicoder.me/problems/no/963"
+
+#include "../competitive-template.hpp"
+#include "../fps/differential-equation.hpp"
+#include "../fps/ntt-friendly-fps.hpp"
+#include "../modint/montgomery-modint.hpp"
+
+using mint = LazyMontgomeryModInt<1012924417>;
+using vm = vector<mint>;
+using fps = FormalPowerSeries<mint>;
+
+void solve() {
+  ini(N);
+  auto g = [](const fps &f, int deg) {
+    return ((f * f + 1) * mint(2).inverse()).pre(deg);
+  };
+  auto gp = [](const fps &f, int deg) { return f.pre(deg); };
+  auto f = DifferentialEquation<mint>(g, gp, 1, N + 1);
+  rep1(i, N) f[N] *= i;
+  out(f[N] * 2);
+}
