0aaebd2031ee7103627f725b28260f568efa1e96
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-04 16:28:18 +0900

    add taylor shift

---
+++ fps/taylor-shift.hpp
@@ -0,0 +1,19 @@
+#include "./formal-power-series.hpp"
+#include "../modulo/binomial.hpp"
+
+// calculate F(x + a)
+template <typename mint>
+FormalPowerSeries<mint> TaylorShift(FormalPowerSeries<mint> f, mint a,
+                                    Binomial<mint>& C) {
+  using fps = FormalPowerSeries<mint>;
+  assert(C.fac_.size() >= f.size() + 1);
+  int N = f.size();
+  for (int i = 0; i < N; i++) f[i] *= C.fac(i);
+  reverse(begin(f), end(f));
+  fps g(N, mint(1));
+  for (int i = 1; i < N; i++) g[i] = g[i - 1] * a * C.inv(i);
+  f = (f * g).pre(N);
+  reverse(begin(f), end(f));
+  for (int i = 0; i < N; i++) f[i] *= C.finv(i);
+  return f;
+}
---
+++ verify/verify-yosupo-fps/yosupo-taylor-shift.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/polynomial_taylor_shift"
+
+#include "../../competitive-template.hpp"
+#include "../../fps/ntt-friendly-fps.hpp"
+#include "../../misc/fastio.hpp"
+#include "../../modint/montgomery-modint.hpp"
+#include "../../modulo/binomial.hpp"
+#include "../../fps/taylor-shift.hpp"
+
+constexpr int MOD9 = 998244353;
+constexpr int MOD1 = 1000000007;
+using mint = LazyMontgomeryModInt<MOD9>;
+Binomial<mint> C(530000);
+using fps = FormalPowerSeries<mint>;
+
+void solve() {
+  int N, c;
+  rd(N);
+  rd(c);
+  fps f(N);
+  rep(i, N) {
+    int buf;
+    rd(buf);
+    f[i] = buf;
+  }
+  fps g = TaylorShift(f, mint(c), C);
+  rep(i,N){
+    if(i)wt(' ');
+    wt(g[i].get());
+  }
+  wt('\n');
+}
