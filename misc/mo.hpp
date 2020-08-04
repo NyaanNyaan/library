0b3995100a86b04bb5f3586e8fd919e647bdd532
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-04 15:27:09 +0900

    add verify

--- misc/mo.hpp
+++ misc/mo.hpp
@@ -4,10 +4,10 @@ using namespace std;
 
 struct Mo {
   int width;
-  vector< int > left, right, order;
-  vector< bool > v;
+  vector<int> left, right, order;
 
-  Mo(int N, int Q) : width((int) sqrt(N)), order(Q), v(N) {
+  Mo(int N, int Q) : order(Q) {
+    width = max(1, N / max<int>(1, sqrt(Q / 3)));
     iota(begin(order), end(order), 0);
   }
 
@@ -16,26 +16,22 @@ struct Mo {
     right.emplace_back(r);
   }
 
-  template<typename ADD,typename DEL,typename REM>
-  void run(const ADD &add, const DEL &del, const REM &rem) {
+  template <typename AL, typename AR, typename DL, typename DR, typename REM>
+  void run(const AL &add_left, const AR &add_right, const DL &delete_left,
+           const DR &delete_right, const REM &rem) {
     assert(left.size() == order.size());
     sort(begin(order), end(order), [&](int a, int b) {
       int ablock = left[a] / width, bblock = left[b] / width;
-      if(ablock != bblock) return ablock < bblock;
-      if(ablock & 1) return right[a] < right[b];
+      if (ablock != bblock) return ablock < bblock;
+      if (ablock & 1) return right[a] < right[b];
       return right[a] > right[b];
     });
     int nl = 0, nr = 0;
-    auto push = [&](int idx) {
-      v[idx].flip();
-      if(v[idx]) add(idx);
-      else del(idx);
-    };
-    for(auto idx : order) {
-      while(nl > left[idx]) push(--nl);
-      while(nr < right[idx]) push(nr++);
-      while(nl < left[idx]) push(nl++);
-      while(nr > right[idx]) push(--nr);
+    for (auto idx : order) {
+      while (nl > left[idx]) add_left(--nl);
+      while (nr < right[idx]) add_right(nr++);
+      while (nl < left[idx]) delete_left(nl++);
+      while (nr > right[idx]) delete_right(--nr);
       rem(idx);
     }
   }
---
+++ verify/verify-yosupo-ds/yosupo-static-range-inversions-query.test.cpp
@@ -0,0 +1,50 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_inversions_query"
+
+#include "../../competitive-template.hpp"
+#include "../../data-structure/binary-indexed-tree.hpp"
+#include "../../misc/compress.hpp"
+#include "../../misc/fastio.hpp"
+#include "../../misc/mo.hpp"
+
+void solve() {
+  int N, Q;
+  rd(N);
+  rd(Q);
+  vi a(N);
+  rep(i, N) rd(a[i]);
+  Mo mo(N, Q);
+  rep(i, Q) {
+    int l, r;
+    rd(l);
+    rd(r);
+    mo.insert(l, r);
+  }
+  compress<int> zip(a);
+  BinaryIndexedTree<int> bit(zip.size() + 1);
+  rep(i, N) a[i] = zip.get(a[i]);
+
+  ll cnt = 0;
+  vl ans(Q);
+  auto addleft = [&](int idx) {
+    cnt += bit.sum(0, a[idx] - 1);
+    bit.add(a[idx], 1);
+  };
+  auto addright = [&](int idx) {
+    cnt += bit.sum(a[idx] + 1, zip.size());
+    bit.add(a[idx], 1);
+  };
+  auto delleft = [&](int idx) {
+    cnt -= bit.sum(0, a[idx] - 1);
+    bit.add(a[idx], -1);
+  };
+  auto delright = [&](int idx) {
+    cnt -= bit.sum(a[idx] + 1, zip.size());
+    bit.add(a[idx], -1);
+  };
+  auto rem = [&](int idx) { ans[idx] = cnt; };
+  mo.run(addleft,addright,delleft,delright,rem);
+  each(x, ans) {
+    wt(x);
+    wt('\n');
+  }
+}
