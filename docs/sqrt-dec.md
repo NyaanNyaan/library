01c45cb27d7d4760f1e2c8ad01292f98978a4849
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-07 01:38:52 +0900

    add docs

---
+++ docs/sqrt-dec.md
@@ -0,0 +1,57 @@
+## 平方分割
+
+要素数$N$の列を$\mathrm{O}(\sqrt(N))$個のバケットに分割して管理することで列上のクエリを高速に答えるアルゴリズムを平方分割と呼ぶ。
+
+セグメント木にない特長として、バケット上に重たいデータ構造を乗せることが出来るという点がある。例えば次の二つのクエリに答える問題を考える。
+
+- `add(l,r,x)` : $[a_l,a_r)$に$x$を足す
+- `count[l,r,x)` : $[a_l,a_r)$内にある$x$の個数を数える
+- $1 \leq l < r \leq N=10^5,$クエリ数$Q=10^5,a_i \leq 10^9, x \leq 10^9$
+
+この問題にセグメント木を適用するとノード上に大量のhashmapを乗せることになり計算量が悪化するが、平方分割ならばhashmapの空間計算量が$\mathrm{O}(N)$で抑えられるため$\mathrm{O}((N+Q)\sqrt{N})$で解くことが出来る。(range addは適切に遅延評価すればよい。)
+
+#### 使い方
+
+次のようなblockを作成して本体に乗せると上手く動く。
+
+```cpp=
+struct block {
+  // S 作用素の型 T 要素の型
+  using S = affine;
+  using T = affine;
+
+  int i;
+
+  block() {}
+
+  // i ... 何個目のブロックか
+  // i * B + j ... (jをブロック内のidxとして)全体でのidx
+  int idx(int j) const { return i * B + j; }
+  
+  // 変数とブロックの初期化を忘れない！
+  void init(int _) { 
+    i = _; 
+  }
+
+  void build() {
+
+  }
+
+  void update_all(S x) {
+
+  }
+
+  void update_part(int l, int r, S x) { 
+    
+    build(); 
+  }
+
+  T query_all() {
+
+  }
+
+  T query_part(int l, int r) {
+
+  }
+};
+```
---
+++ verify/verify-yosupo-ds/yosupo-range-affine-sqdec.test.cpp
@@ -0,0 +1,83 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include "../../competitive-template.hpp"
+#include "../../data-structure/square-decomposition.hpp"
+#include "../../modint/montgomery-modint.hpp"
+
+constexpr int B = 300;
+using mint = LazyMontgomeryModInt<998244353>;
+struct affine {
+  mint a, b;
+  affine() = default;
+  affine(mint a_, mint b_) : a(a_), b(b_) {}
+};
+affine operator*(const  affine &l,const affine &r) {
+  return affine{r.a * l.a, r.a * l.b + r.b};
+};
+
+int N, Q;
+V<affine> a;
+affine id = {1, 0};
+
+void solve() {
+  in(N, Q);
+  a.resize(N);
+  rep(i, N) in(a[i].a, a[i].b);
+
+  struct block {
+    // S 作用素の型 T 要素の型
+    using S = affine;
+    using T = affine;
+
+    int i;
+
+    block() {}
+
+    // i ... 何個目のブロックか
+    // i * B + j ... (jをブロック内のidxとして)全体でのidx
+    int idx(int j) const { return i * B + j; }
+
+    affine fold;
+
+    // 変数とブロックの初期化を忘れない！
+    void init(int _) {
+      i = _;
+      build();
+    }
+
+    void build() {
+      fold = {1, 0};
+      rep(j, B) if (idx(j) < N) fold = fold * a[idx(j)];
+    }
+
+    void update_all(S) { exit(1); }
+
+    void update_part(int l, int r, S x) {
+      for (int j = l; j < r; j++) a[idx(j)] = x;
+      build();
+    }
+
+    T query_all() { return fold; }
+
+    T query_part(int l, int r) {
+      affine ret = id;
+      for (int i = l; i < r; i++) ret = ret * a[idx(i)];
+      return ret;
+    }
+  };
+
+  auto merge = [](const affine &a,const affine &b) { return a * b; };
+  SquareDecomposition<decltype(merge), block, B> sqd(N, merge, id);
+
+  rep(_, Q) {
+    ini(cmd);
+    if (cmd == 0) {
+      ini(p, c, d);
+      sqd.update(p, p + 1, {c, d});
+    } else {
+      ini(l, r, x);
+      affine sm = sqd.query(l, r);
+      out(sm.a * x + sm.b);
+    }
+  }
+}
