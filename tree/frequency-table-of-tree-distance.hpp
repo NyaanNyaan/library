1a6e061f6f1f449e66fd8ee139c49205421b5bfb
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-11 02:31:15 +0900

    fix bug

--- tree/frequency-table-of-tree-distance.hpp
+++ tree/frequency-table-of-tree-distance.hpp
@@ -15,11 +15,24 @@ struct FrequencyTableOfTreeDistance : CentroidDecomposition<G> {
   FrequencyTableOfTreeDistance(const G &g)
       : CentroidDecomposition<G>(g, false) {}
 
+  vector<long long> count, self;
+
+  void dfs_depth(int cur, int par, int d) {
+    while ((int)count.size() <= d) count.emplace_back(0);
+    while ((int)self.size() <= d) self.emplace_back(0);
+    ++count[d];
+    ++self[d];
+    for (int dst : g[cur]) {
+      if (par == dst || v[dst]) continue;
+      dfs_depth(dst, cur, d + 1);
+    }
+  };
+
   vector<long long> get(int start = 0) {
     queue<int> Q;
     int root = get_centroid(start, -1, get_size(start, -1) / 2);
     Q.push(root);
-    vector<long long> ans, count, self;
+    vector<long long> ans;
     ans.reserve(g.size());
     count.reserve(g.size());
     self.reserve(g.size());
@@ -33,17 +46,7 @@ struct FrequencyTableOfTreeDistance : CentroidDecomposition<G> {
         if (v[c]) continue;
         self.clear();
         Q.emplace(get_centroid(c, -1, get_size(c, -1) / 2));
-        auto dfs = [&](auto dfs_, int cur, int par, int d) -> void {
-          while ((int)count.size() <= d) count.emplace_back(0);
-          while ((int)self.size() <= d) self.emplace_back(0);
-          ++count[d];
-          ++self[d];
-          for (int dst : g[cur]) {
-            if (par == dst || v[dst]) continue;
-            dfs_(dfs_, dst, cur, d + 1);
-          }
-        };
-        dfs(dfs, c, r, 1);
+        dfs_depth(c, r, 1);
         auto self2 = ArbitraryNTT::multiply_i128(self, self);
         while (self2.size() > ans.size()) ans.emplace_back(0);
         for (int i = 0; i < (int)self2.size(); i++) ans[i] -= self2[i];
