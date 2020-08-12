522c9ec0d85a6d1ad419050a60a53fe6e67522fe
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-12 21:35:55 +0900

    add berlekamp massey

---
+++ fps/berlekamp-massey.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename mint>
+vector<mint> BerlekampMassey(const vector<mint> &s) {
+  const int N = (int)s.size();
+  vector<mint> b, c;
+  b.reserve(N + 1);
+  c.reserve(N + 1);
+  b.push_back(mint(1));
+  c.push_back(mint(1));
+  mint y = mint(1);
+  for (int ed = 1; ed <= N; ed++) {
+    int l = int(c.size()), m = int(b.size());
+    mint x = 0;
+    for (int i = 0; i < l; i++) x += c[i] * s[ed - l + i];
+    b.emplace_back(mint(0));
+    m++;
+    if (x == mint(0)) continue;
+    mint freq = x / y;
+    if (l < m) {
+      auto tmp = c;
+      c.insert(begin(c), m - l, mint(0));
+      for (int i = 0; i < m; i++) c[m - 1 - i] -= freq * b[m - 1 - i];
+      b = tmp;
+      y = x;
+    } else {
+      for (int i = 0; i < m; i++) c[l - 1 - i] -= freq * b[m - 1 - i];
+    }
+  }
+  reverse(begin(c), end(c));
+  return c;
+}
---
+++ verify/verify-yosupo-fps/yosupo-linear-recurrence.test.cpp
@@ -0,0 +1,23 @@
+#include "../../competitive-template.hpp"
+#include "../../modint/montgomery-modint.hpp"
+#include "../../fps/berlekamp-massey.hpp"
+#include "misc/fastio.hpp"
+
+void solve() {
+  using mint = LazyMontgomeryModInt<998244353>;
+  int N;
+  rd(N);
+  V<mint> a(N);
+  for (int i = 0; i < N; i++) {
+    int n;
+    rd(n);
+    a[i] = n;
+  }
+  auto b = BerlekampMassey<mint>(a);
+  wtn(b.size() - 1);
+  for (int i = 1; i < (int)b.size(); i++) {
+    if (i != 1) wt(' ');
+    wt((-b[i]).get());
+  }
+  wt('\n');
+}
