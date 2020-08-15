a08faa014f159fb789b2c1945c67789c19f19939
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-15 19:54:29 +0900

    add verify

---
+++ data-structure/range-union-find.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+struct RangeUnionFind {
+  vector<int> data, left, right;
+  RangeUnionFind(int N) : data(N, -1) {
+    left.resize(N);
+    right.resize(N);
+    iota(begin(left), end(left), 0);
+    iota(begin(right), end(right), 1);
+  }
+
+  int find(int k) { return data[k] < 0 ? k : data[k] = find(data[k]); }
+
+  int unite(int x, int y) {
+    if ((x = find(x)) == (y = find(y))) return false;
+    if (data[x] > data[y]) swap(x, y);
+    data[x] += data[y];
+    data[y] = x;
+    left[x] = min(left[x], left[y]);
+    right[x] = max(right[x], right[y]);
+    return true;
+  }
+
+  // unite [l, r)
+  void range_unite(int l, int r) {
+    if ((l = max(l, 0)) >= (r = min(r, (int)data.size()))) return;
+    int m;
+    while ((m = right[find(l)]) < r) {
+      assert(left[find(m)] == m);
+      unite(l, m);
+    }
+  }
+
+  int size(int k) { return -data[find(k)]; }
+
+  int same(int x, int y) { return find(x) == find(y); }
+};
---
+++ verify/verify-yuki/yuki-1170.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://yukicoder.me/problems/no/1170"
+
+#include "../../competitive-template.hpp"
+#include "../../data-structure/range-union-find.hpp"
+
+void solve() {
+  inl(N, A, B);
+  vl x(N);
+  in(x);
+  RangeUnionFind uf(N);
+  rep(i, N) {
+    int l = lb(x, x[i] + A);
+    int r = lb(x, x[i] + B + 1);
+    uf.range_unite(l, r);
+  }
+  rep(i, N) {
+    int l = lb(x, x[i] + A);
+    int r = lb(x, x[i] + B + 1);
+    if (l != r) uf.unite(l, i);
+  }
+  rep(i, N) out(uf.size(i));
+}
