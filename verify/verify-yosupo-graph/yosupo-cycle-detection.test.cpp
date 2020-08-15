823b2323bc7e9cf1c5c0493571e17193c0541473
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-15 17:13:54 +0900

    fix bug

--- verify/verify-yosupo-graph/yosupo-cycle-detection.test.cpp
+++ verify/verify-yosupo-graph/yosupo-cycle-detection.test.cpp
@@ -4,50 +4,6 @@
 #include "../../graph/cycle-detection.hpp"
 #include "../../graph/graph-template.hpp"
 
-template <typename G>
-vector<pair<int, int>> CycleDetection(const G& g, bool directed = true) {
-  vector<int> pidx(g.size(), -1), vis(g.size(), 0);
-
-  vector<pair<int, int>> cycle;
-  int finish = 0;
-  auto dfs = [&](auto rec, int cur, int pval, int par) -> int {
-    pidx[cur] = pval;
-    vis[cur] = 1;
-    for (auto& dst : g[cur]) {
-      if (finish) return -1;
-      if (!directed && dst == par) continue;
-      if (pidx[dst] == pidx[cur]) {
-        cycle.emplace_back(cur, dst);
-        return dst;
-      }
-      if (vis[dst]) continue;
-      int nx = rec(rec, dst, pval, cur);
-      trc(cur, dst, nx);
-      if (nx != -1) {
-        cycle.emplace_back(cur, dst);
-        if (cur == nx) {
-          finish = 1;
-          return -1;
-        }
-        return nx;
-      }
-    }
-    pidx[cur] = -1;
-    return -1;
-  };
-
-  for (int i = 0; i < (int)g.size(); i++) {
-    if (vis[i]) continue;
-    dfs(dfs, i, i, -1);
-
-    if (finish) {
-      reverse(begin(cycle), end(cycle));
-      return cycle;
-    }
-  }
-  return vector<pair<int, int>>{};
-}
-
 void solve() {
   ini(N, M);
   vvi g(N);
