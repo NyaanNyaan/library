8d0fd2761e432914615c65ee8e4293e4415a2d72
NyaanNyaan <suteakadapyon3@gmail.com>
2020-08-25 12:15:09 +0900

    add persistent queue

---
+++ data-structure/persistent-queue.hpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+struct PersistentQueue {
+  struct Node {
+    int d;
+    T val;
+    vector<Node*> par;
+    Node(int d_) : d(d_) {}
+    Node(int d_, const T& val_, Node* n) : d(d_), val(val_), par({n}) {}
+  };
+
+  using P = pair<Node*, Node*>;
+  vector<P> roots;
+
+  PersistentQueue() {
+    Node* root = new Node(0);
+    roots.emplace_back(root, root);
+  }
+
+  int push(const T& val, int id = -1) {
+    Node *s, *e;
+    tie(s, e) = id == -1 ? roots.back() : roots[id];
+    Node* ne = new Node(e->d + 1, val, e);
+    roots.emplace_back(s, ne);
+    for (int i = 0;; i++) {
+      if ((int)e->par.size() <= i) break;
+      Node* nxt = e->par[i];
+      ne->par.push_back(e = nxt);
+    }
+    return (int)roots.size() - 1;
+  }
+
+  pair<T, int> pop(int id = -1) {
+    Node *s, *e;
+    tie(s, e) = id == -1 ? roots.back() : roots[id];
+    Node* ns = e;
+    for (int x = e->d - s->d - 1; x;) {
+      int d = __builtin_ctz(x);
+      ns = ns->par[d];
+      x -= 1 << d;
+    }
+    roots.emplace_back(ns, e);
+    return make_pair(ns->val, (int)roots.size() - 1);
+  }
+};
---
+++ verify/verify-yosupo-ds/yosupo-persistent-queue.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_queue"
+
+#include "../../competitive-template.hpp"
+#include "../../data-structure/persistent-queue.hpp"
+
+void solve() {
+  PersistentQueue<int> Q;
+  ini(q);
+  vi roots(q + 1);
+  roots[0] = 0;
+  rep(_, q) {
+    ini(cmd);
+    if (cmd) {
+      ini(i);
+      auto p = Q.pop(roots[i + 1]);
+      out(p.first);
+      roots[_ + 1] = p.second;
+    } else {
+      ini(t, x);
+      auto rts = Q.push(x, roots[t + 1]);
+      roots[_ + 1] = rts;
+    }
+  }
+}
