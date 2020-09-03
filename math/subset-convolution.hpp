54712acc201d018b0f2141a490cb68a529ace9b0
NyaanNyaan <suteakadapyon3@gmail.com>
2020-09-04 01:02:15 +0900

    add subset conv

---
+++ math/subset-convolution.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename mint>
+vector<mint> subset_convolution(const vector<mint>& a, const vector<mint>& b) {
+  int N = a.size();
+  assert(a.size() == b.size() && (N & (N - 1)) == 0);
+  int l = __builtin_ctz(N);
+
+  auto conv = [&](vector<mint>& a, vector<mint>& b) -> void {
+    for (int k = l; k >= 0; --k) {
+      mint n = mint(0);
+      for (int i = 0; i <= k; ++i) n += a[i] * b[k - i];
+      a[k] = n;
+    }
+  };
+
+  vector<vector<mint>> A(N, vector<mint>(l + 1)), B(N, vector<mint>(l + 1));
+  for (int i = 0; i < N; i++) A[i][0] = a[i], B[i][0] = b[i];
+  vector<mint> buf(l + 1);
+
+  for (int n = N / 2, d = 1; n; n >>= 1, ++d) {
+    for (int i = 0; i < N; i += 2 * n) {
+      for (int j = i; j < i + n; j++) {
+        // A[j + n] = A[j] + A[j + n] * x
+        buf[0] = A[j][0];
+        for (int k = 1; k <= d; k++) buf[k] = A[j][k] + A[j + n][k - 1];
+        copy(begin(buf), end(buf), begin(A[j + n]));
+        buf[0] = B[j][0];
+        for (int k = 1; k <= d; k++) buf[k] = B[j][k] + B[j + n][k - 1];
+        copy(begin(buf), end(buf), begin(B[j + n]));
+      }
+    }
+  }
+
+  for (int i = 0; i < N; i++) conv(A[i], B[i]);
+  
+  for (int n = 1, d = 0; n < N; n <<= 1, ++d) {
+    for (int i = 0; i < N; i += 2 * n) {
+      for (int j = i; j < i + n; j++) {
+        // A[j + n] -= A[j]
+        for (int k = d; k <= l; k++) A[j + n][k] -= A[j][k];
+        // A[j] *= x
+        for (int k = l; k > d; --k) A[j][k] = A[j][k - 1];
+        A[j][0] = mint(0);
+      }
+    }
+  }
+
+  vector<mint> C(N);
+  for (int i = 0; i < N; i++) C[i] = A[i][l];
+  return C;
+}
+
+/**
+ * @brief Subset Convolution
+ * @docs docs/math/subset-convolution.md
+ */
---
+++ verify/verify-yosupo-math/yosupo-subset-convolution.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM
+
+#include "../../competitive-template.hpp"
+#include "../../math/subset-convolution.hpp"
+#include "../../misc/fastio.hpp"
+#include "../../modint/montgomery-modint.hpp"
+
+void solve() {
+  int N;
+  rd(N);
+  using mint = LazyMontgomeryModInt<998244353>;
+  using vm = V<mint>;
+  vm a(1 << N), b(1 << N);
+  int n;
+  rep(i, 1 << N) {
+    rd(n);
+    a[i] = n;
+  }
+  rep(i, 1 << N) {
+    rd(n);
+    b[i] = n;
+  }
+  auto c = subset_convolution(a, b);
+  rep(i, 1 << N) {
+    if (i) wt(' ');
+    wt(c[i].get());
+  }
+  wt('\n');
+}
