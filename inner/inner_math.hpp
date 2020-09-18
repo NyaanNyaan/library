aa3df01112ec2b3c60f0447cdb912d8f876d71bf
NyaanNyaan <suteakadapyon3@gmail.com>
2020-09-19 00:42:31 +0900

    fix bug

--- inner/inner_math.hpp
+++ inner/inner_math.hpp
@@ -19,8 +19,8 @@ template <typename T>
 T inv(T a, T p) {
   T b = p, x = 1, y = 0;
   while (a) {
-    T q = b % a;
-    swap(a, b /= a);
+    T q = b / a;
+    swap(a, b %= a);
     swap(x, y -= q * x);
   }
   assert(b == 1);
@@ -29,7 +29,7 @@ T inv(T a, T p) {
 
 template <typename T, typename U>
 T modpow(T a, U n, T p) {
-  T ret = 1;
+  T ret = 1 % p;
   for (; n; n >>= 1, a = U(a) * a % p)
     if (n & 1) ret = U(ret) * a % p;
   return ret;
