0ee28a5de1de41dd538e03026f1e92d1fe8a8f26
NyaanNyaan <suteakadapyon3@gmail.com>
2020-09-20 20:42:18 +0900

    add hash map

---
+++ data-structure/hash-map.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename Key, typename Val, uint32_t N = 1 << 20>
+struct HashMap {
+  using u32 = uint32_t;
+  using u64 = uint64_t;
+
+ private:
+  struct Node {
+    Key key;
+    Val val;
+    Node* nxt;
+    Node() = default;
+  };
+
+  u32 mask;
+  Node** table;
+  Node* pool;
+  int pid;
+
+  Node* my_new(const Key& key, const Val& val) {
+    pool[pid].key = key;
+    pool[pid].val = val;
+    pool[pid].nxt = nullptr;
+    return &(pool[pid++]);
+  }
+
+  u32 hash(u64 x) {
+    static const u64 m =
+        chrono::duration_cast<chrono::nanoseconds>(
+            chrono::high_resolution_clock::now().time_since_epoch())
+            .count();
+    x ^= x >> 23;
+    x ^= m;
+    x ^= x >> 47;
+    return (x - (x >> 32)) & mask;
+  }
+
+ public:
+  HashMap() : mask(N - 1), table(new Node*[N]()), pool(new Node[N]), pid(0) {}
+
+  Val& operator[](const Key& key) {
+    u32 h = hash(key);
+    Node** pp = &(table[h]);
+    while (*pp != nullptr && (*pp)->key != key) pp = &((*pp)->nxt);
+    if (*pp == nullptr) *pp = my_new(key, Val(0));
+    return (*pp)->val;
+  }
+};
+
+/**
+ * @brief Hash Map
+ * @docs data-structure/hash-map.md
+ */
---
+++ verify/verify-yosupo-ds/yosupo-hash-map.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include "../../competitive-template.hpp"
+#include "../../data-structure/hash-map.hpp"
+#include "../../misc/fastio.hpp"
+
+void solve() {
+  HashMap<ll, ll, 1 << 20> m;
+  int Q;
+  ll c, k, v;
+  rd(Q);
+  rep(_, Q) {
+    rd(c);
+    if (c) {
+      rd(k);
+      wtn(m[k]);
+    } else {
+      rd(k, v);
+      m[k] = v;
+    }
+  }
+}
