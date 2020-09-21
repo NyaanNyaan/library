2df6cfa18ca8a70e04c56c836c9934b224f662c3
NyaanNyaan <suteakadapyon3@gmail.com>
2020-09-21 14:00:42 +0900

    add hash map open addressing

---
+++ data-structure/hash-map-chain.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename Key, typename Val, uint32_t N = 1 << 20,
+          Val DefaultValue = Val()>
+struct HashMap {
+  using u32 = uint32_t;
+  using u64 = uint64_t;
+
+ private:
+  struct Node {
+    Key key;
+    Val val;
+    Node* nxt;
+    Node() = default;
+  };
+
+  u32 mask;
+  Node** table;
+  Node* pool;
+  int pid;
+  static constexpr u32 shift = 64 - __lg(N);
+
+  Node* my_new(const Key& key, const Val& val) {
+    pool[pid].key = key;
+    pool[pid].val = val;
+    pool[pid].nxt = nullptr;
+    return &(pool[pid++]);
+  }
+
+  static u64 rng() {
+    u64 m = chrono::duration_cast<chrono::nanoseconds>(
+                chrono::high_resolution_clock::now().time_since_epoch())
+                .count();
+    m ^= m >> 16;
+    m ^= m << 32;
+    return m;
+  }
+
+ public:
+  HashMap() : mask(N - 1), table(new Node*[N]()), pool(new Node[N]), pid(0) {}
+
+  Val& operator[](const Key& key) {
+    static u64 r = rng();
+    u32 h = (u64(key) * r) >> shift;
+    Node** pp = &(table[h]);
+    while (*pp != nullptr && (*pp)->key != key) pp = &((*pp)->nxt);
+    if (*pp == nullptr) *pp = my_new(key, DefaultValue);
+    return (*pp)->val;
+  }
+};
+
+/**
+ * @brief Hash Map(連鎖法)
+ * @docs docs/data-structure/hash-map.md
+ */
--- data-structure/hash-map.hpp
+++ data-structure/hash-map.hpp
@@ -2,55 +2,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <typename Key, typename Val, uint32_t N = 1 << 20>
+template <typename Key, typename Val, uint32_t N = 1 << 20,
+          Val DefaultValue = Val(), Key UnusedKey = numeric_limits<Key>::max()>
 struct HashMap {
   using u32 = uint32_t;
   using u64 = uint64_t;
 
  private:
-  struct Node {
-    Key key;
-    Val val;
-    Node* nxt;
-    Node() = default;
-  };
+  vector<Key> keys;
+  vector<Val> vals;
+  bitset<N> flag;
+  static constexpr u32 shift = 64 - __lg(N);
 
-  u32 mask;
-  Node** table;
-  Node* pool;
-  int pid;
-
-  Node* my_new(const Key& key, const Val& val) {
-    pool[pid].key = key;
-    pool[pid].val = val;
-    pool[pid].nxt = nullptr;
-    return &(pool[pid++]);
-  }
-
-  u32 hash(u64 x) {
-    static const u64 m =
-        chrono::duration_cast<chrono::nanoseconds>(
-            chrono::high_resolution_clock::now().time_since_epoch())
-            .count();
-    x ^= x >> 23;
-    x ^= m;
-    x ^= x >> 47;
-    return (x - (x >> 32)) & mask;
+  static u64 rng() {
+    u64 m = chrono::duration_cast<chrono::nanoseconds>(
+                chrono::high_resolution_clock::now().time_since_epoch())
+                .count();
+    m ^= m >> 16;
+    m ^= m << 32;
+    return m;
   }
 
  public:
-  HashMap() : mask(N - 1), table(new Node*[N]()), pool(new Node[N]), pid(0) {}
+  HashMap() : keys(N, UnusedKey), vals(N, DefaultValue) {
+    static_assert((N & (N - 1)) == 0 && N > 0);
+  }
 
-  Val& operator[](const Key& key) {
-    u32 h = hash(key);
-    Node** pp = &(table[h]);
-    while (*pp != nullptr && (*pp)->key != key) pp = &((*pp)->nxt);
-    if (*pp == nullptr) *pp = my_new(key, Val(0));
-    return (*pp)->val;
+  Val& operator[](const Key& i) {
+    static u64 r = rng();
+    u32 hash = (u64(i) * r) >> shift;
+    while(true){
+      if(!flag[hash]) {
+        keys[hash] = i;
+        flag[hash] = 1;
+        return vals[hash];
+      }
+      if(keys[hash] == i) return vals[hash];
+      hash = (hash + 1) & (N - 1);
+    }
   }
 };
 
 /**
- * @brief Hash Map
+ * @brief Hash Map(開番地法)
  * @docs docs/data-structure/hash-map.md
  */
