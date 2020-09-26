2b8b549e8daaff4ea367f4dc6e0860645b88e52f
NyaanNyaan <suteakadapyon3@gmail.com>
2020-09-27 00:39:08 +0900

    fix bug

--- segment-tree/range-tree.hpp
+++ segment-tree/range-tree.hpp
@@ -5,7 +5,7 @@ using namespace std;
 // DS ... pointer ?
 // T ... value_type
 template <typename DS, typename T>
-struct SegmentTree2D {
+struct FractionalCascading {
   using NEW = function<DS(void)>;
   using ADD = function<void(DS&, int, const T&)>;
   using SUM = function<T(DS&, int, int)>;
@@ -15,12 +15,11 @@ struct SegmentTree2D {
   const ADD ds_add;
   const SUM ds_sum;
   vector<DS> dat;
-  SegmentTree2D(int n, int m, const NEW& nw, const ADD& ad, const SUM& sm)
+
+  FractionalCascading(int n, int m, const NEW& nw, const ADD& ad, const SUM& sm)
       : N(n), M(m), ds_new(nw), ds_add(ad), ds_sum(sm) {
     for (int i = 0; i < N; i++) dat.push_back(ds_new);
   }
 
-  void add(int i,const T& x) {
-    
-  }
+  
 };
