40c15d9af05ca8f35c74ac2e15c175ec8fbf997d
NyaanNyaan <suteakadapyon3@gmail.com>
2020-10-01 14:45:37 +0900

    speed up gcd

--- trial/fast-gcd.hpp
+++ trial/fast-gcd.hpp
@@ -6,17 +6,18 @@ namespace fast_gcd {
 using u64 = uint64_t;
 using u32 = uint32_t;
 
-__attribute__((target("bmi"))) uint64_t binary_gcd(uint64_t a, uint64_t b) {
+__attribute__((target("bmi"))) u64 binary_gcd(u64 a, u64 b) {
   if (a == 0 || b == 0) return a + b;
   int n = __builtin_ctzll(a);
   int m = __builtin_ctzll(b);
   a >>= n;
   b >>= m;
   while (a != b) {
+    int m = __builtin_ctzll(a - b);
     bool f = a > b;
     u64 c = f ? a : b;
     b = f ? b : a;
-    a = (c - b) >> __builtin_ctzll(c - b);
+    a = (c - b) >> m;
   }
   return a << min(n, m);
 }
@@ -57,8 +58,8 @@ void test_inner(u64 (*f)(u64, u64)) {
 
   rng_init();
   test(f, 1e7, 1LL << 16, "small");
-  test(f, 5e6, 1LL << 32, "medium");
-  test(f, 2.5e6, 0, "large");
+  test(f, 1e7, 1LL << 32, "medium");
+  test(f, 1e7, 0, "large");
 }
 
 void unit_test() {
