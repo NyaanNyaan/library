57d207bae84116b9780e197afcf35e8a8e36a5be
NyaanNyaan <suteakadapyon3@gmail.com>
2020-10-05 19:26:40 +0900

    fix bug

--- data-structure-2d/fenwick-tree-on-wavelet-matrix.hpp
+++ data-structure-2d/fenwick-tree-on-wavelet-matrix.hpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 struct bit_vector {
-#pragma GCC target("bmi2", "popcnt")
   using u32 = uint32_t;
   using i64 = int64_t;
   using u64 = uint64_t;
@@ -20,13 +19,13 @@ struct bit_vector {
 
   bit_vector() {}
   bit_vector(int _n) { init(_n); }
-  __attribute__((optimize("O3", "unroll-loops"))) void init(int _n) {
+  __attribute__((optimize("O3,unroll-loops"))) void init(int _n) {
     n = zeros = _n;
     block.resize(n / w + 1, 0);
     count.resize(block.size(), 0);
   }
-  
-  void build() {
+
+  __attribute__((target("popcnt"))) void build() {
     for (u32 i = 1; i < block.size(); ++i)
       count[i] = count[i - 1] + _mm_popcnt_u64(block[i - 1]);
     zeros = rank0(n);
@@ -34,7 +33,7 @@ struct bit_vector {
 
   inline u32 rank0(u32 i) const { return i - rank1(i); }
 
-  inline u32 rank1(u32 i) const {
+  __attribute__((target("bmi2,popcnt"))) inline u32 rank1(u32 i) const {
     return count[i / w] + _mm_popcnt_u64(_bzhi_u64(block[i / w], i % w));
   }
 };
