8690b70220ef1df48967e67f40a0d7b11869b398
NyaanNyaan <suteakadapyon3@gmail.com>
2020-10-21 00:35:35 +0900

    fix bug

--- graph/dimension-expanded-graph.hpp
+++ graph/dimension-expanded-graph.hpp
@@ -81,15 +81,20 @@ struct DimensionExpandedGraph {
   template <typename F>
   vector<i64> bfs01(F f, A s = {}) {
     vector<i64> dist(N, -1);
+    vector<bool> vis(N, 0);
     deque<A> Q;
     dist[id(s)] = 0;
     Q.push_back(s);
     while (!Q.empty()) {
       A c = Q.front();
       Q.pop_front();
+      if (vis[id(c)]) continue;
+      vis[id(c)] = true;
+      i64 dc = dist[id(c)];
       f(c, [&](A d, i64 w) {
-        if (dist[id(d)] == -1) {
-          dist[id(d)] = dist[id(c)] + w;
+        i64 &dd = dist[id(d)];
+        if (dd == -1 || dc + w < dd) {
+          dd = dc + w;
           if (w == 0)
             Q.push_front(d);
           else
@@ -115,52 +120,6 @@ struct DimensionExpandedGraph {
       f(c, [&](A d, i64 w) {
         if (dist[id(d)] == -1 or dist[id(d)] > dist[id(c)] + w) {
           Q.emplace(dist[id(d)] = dist[id(c)] + w, d);
-        }
-      });
-    }
-    return dist;
-  }
-};
-
-struct GridGraph : DimensionExpandedGraph<2> {
-  vector<string> str;
-  char ng;
-  int is_8_direction;
-
-  template <typename... T>
-  GridGraph(const T &... t) : ng('#'), is_8_direction(false) {
-    N = 1;
-    g_size = A({t...});
-    set_coeff();
-  }
-
-  operator vector<string> &() { return str; }
-
-  template <typename F>
-  void adjacent(pair<int,int> c, F f) {
-    int dx[] = {1, 0, -1, 0, 1, 1, -1, -1};
-    int dy[] = {0, 1, 0, -1, 1, -1, 1, -1};
-    int i, j;
-    tie(i, j) = c;
-    for (int k = 0; k < is_8_direction ? 8 : 4; k++) {
-      int di = i + dx[k], dj = dy[k];
-      if (ok(di, dj) and str[di][dj] != ng) f(di, dj);
-    }
-  }
-
-  template <typename F>
-  vector<i64> bfs(F f, A s = {}){
-    vector<i64> dist(N, -1);
-    queue<A> Q;
-    dist[id(s)] = 0;
-    Q.push(s);
-    while (!Q.empty()) {
-      A c = Q.front();
-      Q.pop();
-      adjacent(c, [&](A d) {
-        if (dist[id(d)] == -1) {
-          dist[id(d)] = dist[id(c)] + 1;
-          Q.push(d);
         }
       });
     }
