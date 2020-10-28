f0b8ce7c6d092b8c7da4d6199417d306d5b2fe55
NyaanNyaan <suteakadapyon3@gmail.com>
2020-10-28 22:59:17 +0900

    refine zeta moebius

--- math/zeta-mobius-transform.hpp
+++ math/zeta-mobius-transform.hpp
@@ -5,6 +5,7 @@ using namespace std;
 template <typename T>
 void superset_zeta_transform(vector<T>& f) {
   int n = f.size();
+  assert((n & (n - 1)) == 0);
   for (int i = 1; i < n; i <<= 1) {
     for (int j = 0; j < n; j++) {
       if ((j & i) == 0) {
@@ -17,6 +18,7 @@ void superset_zeta_transform(vector<T>& f) {
 template <typename T>
 void superset_mobius_transform(vector<T>& f) {
   int n = f.size();
+  assert((n & (n - 1)) == 0);
   for (int i = 1; i < n; i <<= 1) {
     for (int j = 0; j < n; j++) {
       if ((j & i) == 0) {
@@ -29,6 +31,7 @@ void superset_mobius_transform(vector<T>& f) {
 template <typename T>
 void subset_zeta_transform(vector<T>& f) {
   int n = f.size();
+  assert((n & (n - 1)) == 0);
   for (int i = 1; i < n; i <<= 1) {
     for (int j = 0; j < n; j++) {
       if ((j & i) == 0) {
@@ -41,6 +44,7 @@ void subset_zeta_transform(vector<T>& f) {
 template <typename T>
 void subset_mobius_transform(vector<T>& f) {
   int n = f.size();
+  assert((n & (n - 1)) == 0);
   for (int i = 1; i < n; i <<= 1) {
     for (int j = 0; j < n; j++) {
       if ((j & i) == 0) {
