09fdd1d389ef34eb024bc62462dcd14be348eeee
NyaanNyaan <suteakadapyon3@gmail.com>
2020-12-08 17:05:55 +0900

    fix

--- verify/verify-unit-test/mf.test.cpp
+++ verify/verify-unit-test/mf.test.cpp
@@ -27,16 +27,16 @@ void test_moebius(int n = TEN(7)) {
   Timer timer;
   // moebius function
   {
-    cerr << "moebius, " << get_name(typeid(T)) << endl;
+    // cerr << "moebius, " << get_name(typeid(T)) << endl;
     timer.reset();
     auto m1 = mobius_function<T>(n);
-    cerr << "emf : " << timer.elapsed() << endl;
+    // cerr << "emf : " << timer.elapsed() << endl;
 
     timer.reset();
     vector<T> m2(n + 1);
     m2[1] = 1;
     divisor_transform::mobius_transform(m2);
-    cerr << "dmt : " << timer.elapsed() << endl;
+    // cerr << "dmt : " << timer.elapsed() << endl;
     assert(m1 == m2);
   }
 }
@@ -46,17 +46,16 @@ void test_sigma0(int n = TEN(7)) {
   Timer timer;
   // divisor function
   {
-    cerr << "sigma0, " << get_name(typeid(T)) << endl;
+    // cerr << "sigma0, " << get_name(typeid(T)) << endl;
     timer.reset();
     auto m1 = sigma0<T>(n);
-    cerr << "emf : " << timer.elapsed() << endl;
+    // cerr << "emf : " << timer.elapsed() << endl;
 
     timer.reset();
     vector<T> m2(n + 1, 1);
     m2[0] = 0;
     divisor_transform::zeta_transform(m2);
-    cerr << "dzt : " << timer.elapsed() << endl;
-
+    // cerr << "dzt : " << timer.elapsed() << endl;
     assert(m1 == m2);
   }
 }
@@ -66,17 +65,16 @@ void test_sigma1(int n = TEN(7)) {
   Timer timer;
   // divisor function
   {
-    cerr << "sigma1, " << get_name(typeid(T)) << endl;
+    // cerr << "sigma1, " << get_name(typeid(T)) << endl;
     timer.reset();
     auto m1 = sigma1<T>(n);
-    cerr << "emf : " << timer.elapsed() << endl;
+    // cerr << "emf : " << timer.elapsed() << endl;
 
     timer.reset();
     vector<T> m2(n + 1);
     rep(i, n + 1) m2[i] = i;
     divisor_transform::zeta_transform(m2);
-    cerr << "dzt : " << timer.elapsed() << endl;
-
+    // cerr << "dzt : " << timer.elapsed() << endl;
     assert(m1 == m2);
   }
 }
@@ -86,35 +84,40 @@ void test_totient(int n = TEN(7)) {
   Timer timer;
   // divisor function
   {
-    cerr << "totient, " << get_name(typeid(T)) << endl;
+    // cerr << "totient, " << get_name(typeid(T)) << endl;
     timer.reset();
     auto m1 = totient<T>(n);
-    cerr << "emf : " << timer.elapsed() << endl;
+    // cerr << "emf : " << timer.elapsed() << endl;
 
     timer.reset();
     vector<T> m2(n + 1);
     rep(i, n + 1) m2[i] = i;
     divisor_transform::mobius_transform(m2);
-    cerr << "dzt : " << timer.elapsed() << endl;
+    // cerr << "dzt : " << timer.elapsed() << endl;
 
     assert(m1 == m2);
   }
 }
 
 void Nyaan::solve() {
-  rep(i, 8) {
-    test_moebius<char>(TEN(i));
-    test_moebius<int>(TEN(i));
-    test_moebius<mint>(TEN(i));
+  for (int i : vi{0, 1, 2, 3, TEN(1), TEN(2), TEN(4), TEN(6), 3333333}) {
+    cerr << i << endl;
+    test_moebius<char>(i);
+    test_moebius<int>(i);
+    test_moebius<mint>(i);
 
-    test_sigma0<char>(TEN(i));
-    test_sigma0<int>(TEN(i));
-    test_sigma0<mint>(TEN(i));
+    test_sigma0<char>(i);
+    test_sigma0<int>(i);
+    test_sigma0<mint>(i);
 
-    test_sigma1<int>(TEN(i));
-    test_sigma1<mint>(TEN(i));
+    test_sigma1<int>(i);
+    test_sigma1<mint>(i);
 
-    test_totient<int>(TEN(i));
-    test_totient<mint>(TEN(i));
+    test_totient<int>(i);
+    test_totient<mint>(i);
   }
+
+  int a, b;
+  cin >> a >> b;
+  cout << a + b << endl;
 }
