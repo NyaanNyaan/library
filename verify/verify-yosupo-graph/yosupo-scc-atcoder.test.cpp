7f64f4bdb2c3ef6411fed23aa4c690e8cd0d8f7a
NyaanNyaan <suteakadapyon3@gmail.com>
2020-12-17 22:14:45 +0900

    fix

--- verify/verify-yosupo-graph/yosupo-scc-atcoder.test.cpp
+++ verify/verify-yosupo-graph/yosupo-scc-atcoder.test.cpp
@@ -1,18 +1,19 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/scc"
 
 #include "../../template/template.hpp"
+//
 #include "../../atcoder/scc.hpp"
 
-using namespace Nyaan; void Nyaan::solve() {
+using namespace atcoder;
+using namespace Nyaan;
+void Nyaan::solve() {
   ini(N, M);
   scc_graph scc(N);
-  rep(_,M) {
-    ini(u,v);
-    scc.add_edge(u,v);
+  rep(_, M) {
+    ini(u, v);
+    scc.add_edge(u, v);
   }
   auto res = scc.scc();
   out(res.size());
-  each(x, res) {
-    cout << x.size() << " " << x << endl;
-  }
+  each(x, res) { cout << x.size() << " " << x << endl; }
 }
