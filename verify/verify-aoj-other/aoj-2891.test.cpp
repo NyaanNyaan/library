adbc0000d3056aead86bbcc382947ff03af3ae6c
NyaanNyaan <suteakadapyon3@gmail.com>
2021-04-19 17:09:00 +0900

    add verify

---
+++ verify/verify-aoj-other/aoj-2891.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2891"
+//
+#include "../../template/template.hpp"
+//
+#include "../../graph/funtional-graph.hpp"
+
+using namespace Nyaan;
+
+void Nyaan::solve() {
+  ini(N);
+  Namori<ll> g(N);
+  rep(i, N) {
+    ini(u, v);
+    --u, --v;
+    g.add_edge(u, v);
+  }
+
+  ini(Q);
+  rep(i, Q) {
+    ini(a, b);
+    --a, --b;
+    auto [_, xa] = g.idx(a);
+    auto [__, xb] = g.idx(b);
+    if (xa != 0 or xb != 0)
+      out(1);
+    else
+      out(2);
+  }
+}
