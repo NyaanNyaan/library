0f4fe22dcc45e29fbbe16705b9ae31f5e52f28c9
NyaanNyaan <suteakadapyon3@gmail.com>
2021-12-21 22:44:06 +0900

    refine nim product

--- verify/verify-yosupo-math/yosupo-nim-product.test.cpp
+++ verify/verify-yosupo-math/yosupo-nim-product.test.cpp
@@ -1,15 +1,16 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/nim_product_64"
 
-#include "../../template/template.hpp"
 #include "../../math/nim-product.hpp"
 #include "../../misc/fastio.hpp"
+#include "../../template/template.hpp"
 
-using namespace Nyaan; void Nyaan::solve() {
+using namespace Nyaan;
+void Nyaan::solve() {
   int T;
   rd(T);
   rep(i, T) {
     uint64_t a, b;
     rd(a, b);
-    wtn(nim_product(a, b));
+    wtn(NimberImpl::product64(a, b));
   }
 }
