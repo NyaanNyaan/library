36485cee3d34cf40db09c2e011dd17c639218577
NyaanNyaan <suteakadapyon3@gmail.com>
2022-11-10 01:01:58 +0900

    add slope trick

--- segment-tree/rbst-segment-tree.hpp
+++ segment-tree/rbst-segment-tree.hpp
@@ -15,6 +15,7 @@
   inline constexpr bool has_##var##_v = has_##var<T>::value;
 
 ENABLE_HAS_VAR(lazy);
+ENABLE_HAS_VAR(shift);
 
 template <typename Node, typename I, typename T, typename E, T (*f)(T, T),
           T (*g)(T, E), E (*h)(E, E), T (*ti)(), E (*ei)()>
@@ -22,21 +23,21 @@ struct RBSTSegmentTreeBase {
  protected:
   using Ptr = Node *;
   template <typename... Args>
-  inline Ptr _my_new(Args... args) {
+  static Ptr _my_new(Args... args) {
     return new Node(args...);
   }
-  inline void _my_del(Ptr t) { delete t; }
+  static void _my_del(Ptr t) { delete t; }
 
-  inline int _count(const Ptr t) const { return t ? t->cnt : 0; }
+  static int _count(const Ptr t) { return t ? t->cnt : 0; }
 
-  T _sum(const Ptr &t) { return t ? t->sum : ti(); }
+  static T _sum(const Ptr &t) { return t ? t->sum : ti(); }
 
   static uint64_t _rng() {
     static uint64_t x_ = 88172645463325252ULL;
     return x_ ^= x_ << 7, x_ ^= x_ >> 9, x_ & 0xFFFFFFFFull;
   }
 
-  Ptr _merge(Ptr l, Ptr r) {
+  static Ptr _merge(Ptr l, Ptr r) {
     if (!l || !r) return l ? l : r;
     if (int((_rng() * (l->cnt + r->cnt)) >> 32) < l->cnt) {
       _push(l);
@@ -49,14 +50,14 @@ struct RBSTSegmentTreeBase {
     }
   }
 
-  Ptr _build(int l, int r, const vector<pair<I, T>> &dat) {
+  static Ptr _build(int l, int r, const vector<pair<I, T>> &dat) {
     if (l == r) return nullptr;
     if (l + 1 == r) return _my_new(dat[l].first, dat[l].second);
     int m = (l + r) / 2;
     return _merge(_build(l, m, dat), _build(m, r, dat));
   };
 
-  void _push([[maybe_unused]] Ptr t) {
+  static void _push([[maybe_unused]] Ptr t) {
     if constexpr (has_lazy_v<Node>) {
       if (!t) return;
       if (t->lazy != ei()) {
@@ -65,9 +66,16 @@ struct RBSTSegmentTreeBase {
         t->lazy = ei();
       }
     }
+    if constexpr (has_shift_v<Node>) {
+      if (!t) return;
+      if (t->shift != I{}) {
+        if (t->l) _shift(t->l, t->shift);
+        if (t->r) _shift(t->r, t->shift);
+        t->shift = I{};
+      }
+    }
   }
-
-  void _propagate([[maybe_unused]] Ptr t, [[maybe_unused]] const E &x) {
+  static void _propagate([[maybe_unused]] Ptr t, [[maybe_unused]] const E &x) {
     if constexpr (has_lazy_v<Node>) {
       if (!t) return;
       t->lazy = h(t->lazy, x);
@@ -75,8 +83,14 @@ struct RBSTSegmentTreeBase {
       t->sum = g(t->sum, x);
     }
   }
+  static void _shift([[maybe_unused]] Ptr t, [[maybe_unused]] const I &sh) {
+    if constexpr (has_shift_v<Node>) {
+      if (!t) return;
+      t->key += sh, t->shift += sh;
+    }
+  }
 
-  Ptr _update(Ptr t) {
+  static Ptr _update(Ptr t) {
     if (!t) return t;
     t->cnt = 1;
     t->sum = t->val;
@@ -86,7 +100,7 @@ struct RBSTSegmentTreeBase {
   }
 
   // key が k であるノードを探す, なければ nullptr
-  Ptr _find(Ptr t, I k) {
+  static Ptr _find(Ptr t, I k) {
     while (t) {
       _push(t);
       if (k == t->key) return t;
@@ -95,7 +109,7 @@ struct RBSTSegmentTreeBase {
     return nullptr;
   }
 
-  void _erase(Ptr &t, I k) {
+  static void _erase(Ptr &t, I k) {
     if (!t) return;
     _push(t);
     if (k == t->key) {
@@ -112,7 +126,7 @@ struct RBSTSegmentTreeBase {
   }
 
   // [k 未満, k 以上]
-  pair<Ptr, Ptr> _split_left(Ptr t, I k) {
+  static pair<Ptr, Ptr> _split_by_key(Ptr t, I k) {
     if (!t) return {nullptr, nullptr};
     _push(t);
     if (k == t->key) {
@@ -120,19 +134,38 @@ struct RBSTSegmentTreeBase {
       t->l = nullptr;
       return {tl, _update(t)};
     } else if (k < t->key) {
-      auto s = _split_left(t->l, k);
+      auto s = _split_by_key(t->l, k);
       t->l = s.second;
       return {s.first, _update(t)};
     } else {
-      auto s = _split_left(t->r, k);
+      auto s = _split_by_key(t->r, k);
       t->r = s.first;
       return {_update(t), s.second};
     }
   }
 
+  // [k 未満, k, k 超過]
+  static array<Ptr, 3> _split_by_key3(Ptr t, I k) {
+    if (!t) return {{nullptr, nullptr, nullptr}};
+    _push(t);
+    if (k == t->key) {
+      Ptr tl = t->l, tr = t->r;
+      t->l = t->r = nullptr;
+      return {{tl, _update(t), tr}};
+    } else if (k < t->key) {
+      auto s = _split_by_key3(t->l, k);
+      t->l = s[2];
+      return {{s[0], s[1], _update(t)}};
+    } else {
+      auto s = _split_by_key3(t->r, k);
+      t->r = s[0];
+      return {{_update(t), s[1], s[2]}};
+    }
+  }
+
   // (-inf, i] の prod について check(prod) の (true / false) で切る
   template <typename C>
-  pair<Ptr, Ptr> _split_by_cond_left(Ptr t, const C &check, T prod = T{}) {
+  static pair<Ptr, Ptr> _split_max_right(Ptr t, const C &check, T prod = ti()) {
     assert(check(prod));
     if (!t) return {nullptr, nullptr};
     _push(t);
@@ -140,7 +173,7 @@ struct RBSTSegmentTreeBase {
     if (check(p1)) {
       prod = p1;
     } else {
-      auto s = _split_by_cond_left(t->l, check, prod);
+      auto s = _split_max_right(t->l, check, prod);
       t->l = s.second;
       return {s.first, _update(t)};
     }
@@ -154,7 +187,7 @@ struct RBSTSegmentTreeBase {
     if (check(p1)) {
       return {t, nullptr};
     } else {
-      auto s = _split_by_cond_left(t->r, check, prod);
+      auto s = _split_max_right(t->r, check, prod);
       t->r = s.first;
       return {_update(t), s.second};
     }
@@ -162,7 +195,7 @@ struct RBSTSegmentTreeBase {
 
   // [i, inf) の prod について check(prod) の (false / true) で切る
   template <typename C>
-  pair<Ptr, Ptr> _split_by_cond_right(Ptr t, const C &check, T prod = T{}) {
+  static pair<Ptr, Ptr> _split_min_left(Ptr t, const C &check, T prod = ti()) {
     assert(check(prod));
     if (!t) return {nullptr, nullptr};
     _push(t);
@@ -170,7 +203,7 @@ struct RBSTSegmentTreeBase {
     if (check(p1)) {
       prod = p1;
     } else {
-      auto s = _split_by_cond_right(t->r, check, prod);
+      auto s = _split_min_left(t->r, check, prod);
       t->r = s.first;
       return {_update(t), s.second};
     }
@@ -184,33 +217,14 @@ struct RBSTSegmentTreeBase {
     if (check(p1)) {
       return {nullptr, t};
     } else {
-      auto s = _split_by_cond_right(t->l, check, prod);
+      auto s = _split_min_left(t->l, check, prod);
       t->l = s.second;
       return {s.first, _update(t)};
     }
   }
 
-  // [k 未満, k, k 超過]
-  array<Ptr, 3> _split_by_key(Ptr t, I k) {
-    if (!t) return {{nullptr, nullptr, nullptr}};
-    _push(t);
-    if (k == t->key) {
-      Ptr tl = t->l, tr = t->r;
-      t->l = t->r = nullptr;
-      return {{tl, _update(t), tr}};
-    } else if (k < t->key) {
-      auto s = _split_by_key(t->l, k);
-      t->l = s[2];
-      return {{s[0], s[1], _update(t)}};
-    } else {
-      auto s = _split_by_key(t->r, k);
-      t->r = s[0];
-      return {{_update(t), s[1], s[2]}};
-    }
-  }
-
   // [l, inf) である地点に apply
-  void _apply_left(Ptr t, I l, const E &e) {
+  static void _apply_left(Ptr t, I l, const E &e) {
     if (!t) return;
     _push(t);
     if (t->key < l) {
@@ -227,7 +241,7 @@ struct RBSTSegmentTreeBase {
   }
 
   // [-inf, r) である地点に apply
-  void _apply_right(Ptr t, I r, const E &e) {
+  static void _apply_right(Ptr t, I r, const E &e) {
     if (!t) return;
     _push(t);
     if (t->key < r) {
@@ -243,7 +257,7 @@ struct RBSTSegmentTreeBase {
   }
 
   // [l, r) に apply
-  void _apply(Ptr t, I l, I r, const E &e) {
+  static void _apply(Ptr t, I l, I r, const E &e) {
     if (!t) return;
     _push(t);
     if (t->key < l) {
@@ -264,7 +278,7 @@ struct RBSTSegmentTreeBase {
   }
 
   // l 以上
-  T _fold_left(Ptr t, I l) {
+  static T _fold_left(Ptr t, I l) {
     if (!t) return ti();
     _push(t);
     if (t->key < l) {
@@ -278,7 +292,7 @@ struct RBSTSegmentTreeBase {
   }
 
   // r 未満
-  T _fold_right(Ptr t, I r) {
+  static T _fold_right(Ptr t, I r) {
     if (!t) return ti();
     _push(t);
     if (t->key < r) {
@@ -291,7 +305,7 @@ struct RBSTSegmentTreeBase {
     }
   }
 
-  T _fold(Ptr t, I l, I r) {
+  static T _fold(Ptr t, I l, I r) {
     if (!t) return ti();
     _push(t);
     if (t->key < l) {
@@ -310,15 +324,15 @@ struct RBSTSegmentTreeBase {
   }
 
   // t を根とする木の上で最小の key は？ (t が空の場合は failed)
-  pair<I, T> _get_min_keyval(Ptr t, const I &failed) {
-    if (t == nullptr) return {failed, ti()};
+  static pair<I, T> _get_min_keyval(Ptr t, const I &failed) {
+    if (!t) return {failed, ti()};
     while (t->l) _push(t), t = t->l;
     return {t->key, t->val};
   }
 
   // t を根とする木の上で最小の key は？ (t が空の場合は failed)
-  pair<I, T> _get_max_keyval(Ptr t, const I &failed) {
-    if (t == nullptr) return {failed, ti()};
+  static pair<I, T> _get_max_keyval(Ptr t, const I &failed) {
+    if (!t) return {failed, ti()};
     while (t->r) _push(t), t = t->r;
     return {t->key, t->val};
   }
@@ -327,8 +341,8 @@ struct RBSTSegmentTreeBase {
   // exclusive かつ (空 または[0,右]が真の場合) の場合は failed(inf)
   // inclusive かつ (空 または[0,0] が偽の場合) の場合は failed
   template <typename C, bool exclusive>
-  I _max_right(Ptr t, C check, const I &failed) {
-    if (t == nullptr) return failed;
+  static I _max_right(Ptr t, C check, const I &failed) {
+    if (!t) return failed;
     _push(t);
     Ptr now = t;
     T prod_now = ti();
@@ -370,8 +384,8 @@ struct RBSTSegmentTreeBase {
   // inclusive かつ (空 または 存在しない) 場合は failed
   // exlucisve かつ (空 または [左, inf) が真) の場合は failed
   template <typename C, bool inclusive>
-  I _min_left(Ptr t, C check, const I &failed) {
-    if (t == nullptr) return failed;
+  static I _min_left(Ptr t, C check, const I &failed) {
+    if (!t) return failed;
     _push(t);
     Ptr now = t;
     T prod_now = ti();
@@ -409,36 +423,40 @@ struct RBSTSegmentTreeBase {
     }
   }
 
-  void _clear(Ptr t) {
+  static void _clear(Ptr t) {
     if (!t) return;
     if (t->l) _clear(t->l);
     if (t->r) _clear(t->r);
     _my_del(t);
   }
 
-  Ptr _copy(Ptr t) {
+  static Ptr _deepcopy(Ptr t) {
     if (!t) return nullptr;
     Ptr u = _my_new(*t);
-    if (u->l) u->l = _copy(u->l);
-    if (u->r) u->r = _copy(u->r);
+    if (u->l) u->l = _deepcopy(u->l);
+    if (u->r) u->r = _deepcopy(u->r);
     return u;
   }
 
-  void _dump(Ptr t) {
+  static void _dump(Ptr t) {
+    if (!t) return;
     _push(t);
-    if (t->l) _dump(t->l);
-    cerr << "## i = " << t->key << ", ";
-    cerr << "\tkey = " << t->val << ", ";
+    _dump(t->l);
+    cerr << "## key = " << t->key << ",";
+    cerr << "\tval = " << t->val << ", ";
     cerr << "\tsum = " << t->sum << ", ";
     cerr << "\tchild = ";
-    cerr << "( " << (t->l ? to_string(t->l->key) : "nil");
+    cerr << "( ";
+    if (t->l) cerr << t->l->key;
+    if (!t->l) cerr << "nil";
     cerr << ", ";
-    cerr << (t->r ? to_string(t->r->key) : "nil");
+    if (t->r) cerr << t->r->key;
+    if (!t->r) cerr << "nil";
     cerr << " )" << endl;
-    if (t->r) _dump(t->r);
+    _dump(t->r);
   }
 
-  void _make_array(Ptr t, vector<pair<I, T>> &v) {
+  static void _make_array(Ptr t, vector<pair<I, T>> &v) {
     if (!t) return;
     _push(t);
     if (t->l) _make_array(t->l, v);
@@ -450,6 +468,7 @@ struct RBSTSegmentTreeBase {
   Ptr root;
 
   RBSTSegmentTreeBase() : root(nullptr) {}
+  RBSTSegmentTreeBase(Ptr t) : root(t) {}
   RBSTSegmentTreeBase(const vector<T> xs, const vector<I> &vals = {}) {
     if (!vals.empty()) assert(xs.size() == vals.size());
     int n = xs.size();
@@ -458,9 +477,7 @@ struct RBSTSegmentTreeBase {
     root = _build(0, n, dat);
   }
   RBSTSegmentTreeBase(RBSTSegmentTreeBase &&rhs) noexcept { root = rhs.root; }
-  RBSTSegmentTreeBase(const RBSTSegmentTreeBase &rhs) {
-    root = _copy(rhs.root);
-  }
+  RBSTSegmentTreeBase(const RBSTSegmentTreeBase &rhs) { root = rhs.root; }
   ~RBSTSegmentTreeBase() = default;
 
   using RBST = RBSTSegmentTreeBase;
@@ -469,9 +486,10 @@ struct RBSTSegmentTreeBase {
     return *this;
   }
   RBST &operator=(const RBST &rhs) {
-    root = _copy(rhs.root);
+    root = rhs.root;
     return *this;
   }
+  RBST deepcopy() { return _deepcopy(root); }
 
   friend void swap(RBST &lhs, RBST &rhs) { swap(lhs.root, rhs.root); }
   void swap(RBST &rhs) { swap(root, rhs.root); }
@@ -479,14 +497,12 @@ struct RBSTSegmentTreeBase {
   // destructive ordered _merge (max(lhs) < min(rhs))
   friend RBST ordered_merge(RBST &lhs, RBST &rhs) {
     assert(lhs.get_max_key() < rhs.get_min_key());
-    RBST res;
-    res.root = res._merge(lhs, rhs);
-    return res;
+    return RBST{_merge(lhs.root, rhs.root)};
   }
 
   // 1 点 値の書き換え
   void set_val(I i, T x) {
-    auto s = _split_by_key(root, i);
+    auto s = _split_by_key3(root, i);
     if (s[1] == nullptr) {
       s[1] = _my_new(i, x);
     } else {
@@ -499,17 +515,12 @@ struct RBSTSegmentTreeBase {
   void set_val_fast(I i, T x) {
     static vector<Ptr> ps;
     ps.clear();
-
     Ptr t = root;
     while (t) {
       _push(t);
       ps.push_back(t);
       if (i == t->key) break;
-      if (i < t->key) {
-        t = t->l;
-      } else {
-        t = t->r;
-      }
+      t = i < t->key ? t->l : t->r;
     }
     if (!t) {
       set_val(i, x);
@@ -528,7 +539,7 @@ struct RBSTSegmentTreeBase {
   // 1 点 値の書き換え
   // func の返り値は void !!!!!!(参照された値を直接更新する)
   void apply_val(I i, const function<void(T &)> &func) {
-    auto s = _split_by_key(root, i);
+    auto s = _split_by_key3(root, i);
     if (s[1] == nullptr) s[1] = _my_new(i);
     func(s[1]->val);
     root = _merge(_merge(s[0], _update(s[1])), s[2]);
@@ -561,6 +572,7 @@ struct RBSTSegmentTreeBase {
     if (l >= r) return;
     _apply(root, l, r, e);
   }
+  void apply_all(const E &e) { _propagate(root, e); }
 
   // 範囲取得
   T fold(I l, I r) {
@@ -568,6 +580,8 @@ struct RBSTSegmentTreeBase {
     return _fold(root, l, r);
   }
   T fold_all() { return _sum(root); }
+
+  void shift(const I &sh) { _shift(root, sh); }
 
   // key 最小を取得
   I get_min_key(I failed = -1) { return _get_min_keyval(root, failed).first; }
@@ -601,7 +615,7 @@ struct RBSTSegmentTreeBase {
   template <typename C>
   I min_left(I n, C check, I failed) {
     assert(check(ti()) == true);
-    auto [x, y] = _split_left(root, n);
+    auto [x, y] = _split_by_key(root, n);
     I res = _min_left<C, true>(x, check, failed);
     root = _merge(x, y);
     return res;
@@ -612,7 +626,7 @@ struct RBSTSegmentTreeBase {
   template <typename C>
   I min_left_exclusive(I n, C check, I minus_infty) {
     assert(check(ti()) == true);
-    auto [x, y] = _split_left(root, n);
+    auto [x, y] = _split_by_key(root, n);
     I res = _min_left<C, false>(x, check, minus_infty);
     root = _merge(x, y);
     return res;
@@ -623,7 +637,7 @@ struct RBSTSegmentTreeBase {
   template <typename C>
   I max_right(I n, C check, I infty) {
     assert(check(ti()) == true);
-    auto [x, y] = _split_left(root, n);
+    auto [x, y] = _split_by_key(root, n);
     I res = _max_right<C, true>(y, check, infty);
     root = _merge(x, y);
     return res;
@@ -634,16 +648,46 @@ struct RBSTSegmentTreeBase {
   template <typename C>
   I max_right_inclusive(I n, C check, I failed) {
     assert(check(ti()) == true);
-    auto [x, y] = _split_left(root, n);
+    auto [x, y] = _split_by_key(root, n);
     I res = _max_right<C, false>(y, check, failed);
     root = _merge(x, y);
     return res;
   }
 
+  // (key 未満, key 以上) で分割
+  // 呼び出し後のオブジェクトは空のセグ木になる
+  pair<RBST, RBST> split_by_key(const I &key) {
+    auto [x, y] = _split_by_key(root, key);
+    root = nullptr;
+    return make_pair(RBST{x}, RBST{y});
+  }
+  // [i, inf) の区間積が (false, true) になる境界で分割
+  // 呼び出し後のオブジェクトは空のセグ木になる
+  template <typename C>
+  pair<RBST, RBST> split_min_left(const C &check) {
+    assert(check(ti()) == true);
+    auto [x, y] = _split_min_left(root, check);
+    root = nullptr;
+    return make_pair(RBST{x}, RBST{y});
+  }
+  // (-inf, i] の区間積が (true, false) になる境界で分割
+  // 呼び出し後のオブジェクトは空のセグ木になる
+  template <typename C>
+  pair<RBST, RBST> split_max_right(const C &check) {
+    assert(check(ti()) == true);
+    auto [x, y] = _split_max_right(root, check);
+    root = nullptr;
+    return make_pair(RBST{x}, RBST{y});
+  }
+
   void clear() { _clear(root), root = nullptr; }
   int size() { return _count(root); }
   bool empty() { return !root; }
-  void dump() { _dump(root); }
+  void dump() {
+    cerr << "***** dump start *****" << endl;
+    _dump(root);
+    cerr << "****** dump end ******" << endl;
+  }
 
   // 列を配列に変換して返す
   vector<pair<I, T>> make_array() {
@@ -654,6 +698,24 @@ struct RBSTSegmentTreeBase {
 };
 
 namespace RBSTSegmentTreeImpl {
+bool _ei() { return false; }
+
+template <typename I, typename T, typename E, T (*f)(T, T), T (*g)(T, E),
+          E (*h)(E, E), T (*ti)(), E (*ei)()>
+struct ShiftableLazySegNode {
+  ShiftableLazySegNode *l, *r;
+  I key, shift;
+  T val, sum;
+  E lazy;
+  int cnt;
+  ShiftableLazySegNode(const I &i, const T &t = ti())
+      : l(), r(), key(i), shift(I{}), val(t), sum(t), lazy(ei()), cnt(1) {}
+};
+template <typename I, typename T, typename E, T (*f)(T, T), T (*g)(T, E),
+          E (*h)(E, E), T (*ti)(), E (*ei)()>
+using RBSTShiftableLazySegmentTree =
+    RBSTSegmentTreeBase<ShiftableLazySegNode<I, T, E, f, g, h, ti, ei>, I, T, E,
+                        f, g, h, ti, ei>;
 
 template <typename I, typename T, typename E, T (*f)(T, T), T (*g)(T, E),
           E (*h)(E, E), T (*ti)(), E (*ei)()>
@@ -672,7 +734,6 @@ using RBSTLazySegmentTree =
     RBSTSegmentTreeBase<LazySegNode<I, T, E, f, g, h, ti, ei>, I, T, E, f, g, h,
                         ti, ei>;
 
-bool ei() { return false; }
 template <typename I, typename T, T (*f)(T, T), T (*ti)()>
 struct SegNode {
   SegNode *l, *r;
@@ -684,12 +745,13 @@ struct SegNode {
 };
 template <typename I, typename T, T (*f)(T, T), T (*ti)()>
 using RBSTSegmentTree = RBSTSegmentTreeBase<SegNode<I, T, f, ti>, I, T, bool, f,
-                                            nullptr, nullptr, ti, ei>;
+                                            nullptr, nullptr, ti, _ei>;
 
 }  // namespace RBSTSegmentTreeImpl
 
 using RBSTSegmentTreeImpl::RBSTLazySegmentTree;
 using RBSTSegmentTreeImpl::RBSTSegmentTree;
+using RBSTSegmentTreeImpl::RBSTShiftableLazySegmentTree;
 
 /**
  * @brief RBST-based Dynamic Lazy Segment Tree
