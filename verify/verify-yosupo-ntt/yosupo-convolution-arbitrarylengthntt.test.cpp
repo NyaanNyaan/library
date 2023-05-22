cbf6e0d4fbd2e580f1060a4c8f21cae42c041336
NyaanNyaan <suteakadapyon3@gmail.com>
2023-05-22 19:07:34 +0900

    fix bug

--- verify/verify-yosupo-ntt/yosupo-convolution-arbitrarylengthntt.test.cpp
+++ verify/verify-yosupo-ntt/yosupo-convolution-arbitrarylengthntt.test.cpp
@@ -1,18 +1,20 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
 
 #include "../../template/template.hpp"
+//
 #include "../../misc/fastio.hpp"
 #include "../../modint/montgomery-modint.hpp"
 #include "../../ntt/cooley-tukey-ntt.hpp"
 
-using namespace Nyaan; void Nyaan::solve() {
+using namespace Nyaan;
+void Nyaan::solve() {
   using mint = LazyMontgomeryModInt<998244353>;
   ArbitraryLengthNTT<mint> ntt(7 * 17 * (1 << 14));
-  int N, M;
+  int N, M, x;
   rd(N, M);
   V<mint> a(N), b(M);
-  for (int i = 0; i < N; i++) rd(a[i]);
-  for (int i = 0; i < M; i++) rd(b[i]);
+  for (int i = 0; i < N; i++) rd(x), a[i] = x;
+  for (int i = 0; i < M; i++) rd(x), b[i] = x;
   auto c = ntt.multiply(a, b);
   for (int i = 0; i < (int)N + M - 1; i++) {
     wt(c[i].get(), (i == (int)N + M - 1 ? '\n' : ' '));
