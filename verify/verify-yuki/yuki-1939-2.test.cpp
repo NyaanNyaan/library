ff6e78685694107cef053f9747b92347f1872151
NyaanNyaan <suteakadapyon3@gmail.com>
2024-03-23 07:28:36 +0900

    fix bug

--- verify/verify-yuki/yuki-1939-2.test.cpp
+++ verify/verify-yuki/yuki-1939-2.test.cpp
@@ -1,11 +1,12 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/composition_of_formal_power_series_large"
+#define PROBLEM "https://yukicoder.me/problems/no/1939"
 //
 #include "../../template/template.hpp"
 //
-#include "../../fps/fps-composition.hpp"
+#include "../../fps/fps-compositional-inverse.hpp"
 //
 #include "../../fps/ntt-friendly-fps.hpp"
 #include "../../modint/montgomery-modint.hpp"
+#include "../../modulo/binomial.hpp"
 // #include "fps/arbitrary-fps.hpp"
 //
 using namespace Nyaan;
@@ -13,16 +14,22 @@ using mint = LazyMontgomeryModInt<998244353>;
 // using mint = LazyMontgomeryModInt<1000000007>;
 using vm = vector<mint>;
 using vvm = vector<vm>;
+Binomial<mint> C;
 using fps = FormalPowerSeries<mint>;
 using namespace Nyaan;
 
 void q() {
-  inl(N);
-  fps g(N), f(N);
-  in(g, f);
-  out(composition(f, g));
-}
+  inl(N, M);
+  vl L(M);
+  in(L);
 
+  fps F(N + 2);
+  F[0] = 1;
+  each(n, L) F[n] = 1;
+  F = (fps{0, 1} * F.inv()).pre(N + 2);
+  auto f = compositional_inverse(F);
+  out(f[N + 1]);
+}
 void Nyaan::solve() {
   int t = 1;
   // in(t);
